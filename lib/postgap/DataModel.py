712ca703d74f7636aa9496ff800715cfdb8b9fae
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-06-06 13:57:18 +0100

    DataModel.py: Added field to store the finemap_posteriors

--- lib/postgap/DataModel.py
+++ lib/postgap/DataModel.py
@@ -68,7 +68,8 @@ GWAS_Cluster = collections.namedtuple(
 	'GWAS_Cluster', 
 	[
 		'gwas_snps',
-		'ld_snps'
+		'ld_snps',
+		'finemap_posteriors'
 	]
 )
 Cisregulatory_Evidence = collections.namedtuple('Cisregulatory_Evidence', ['snp','gene','score','source','study','tissue','info'])
