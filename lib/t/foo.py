52d302c10840091647e3eaaa745166ff4aee3de0
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-06-07 15:50:38 +0100

    Moved test code into unit test

--- lib/t/foo.py
+++ lib/t/foo.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Run with this incantation:
+#
+# python -m unittest discover -s lib/t/ -p "*.py"
+
 import unittest
 
 class Blabladiblupp(unittest.TestCase):
@@ -8,8 +12,26 @@ class Blabladiblupp(unittest.TestCase):
 		pass
  
 	def test_foooooo(self):
-		print "Hello world!"
-		self.assertEqual(2, 2)
- 
+		
+		import postgap.Globals
+		postgap.Globals.DATABASES_DIR = '/nfs/nobackup/ensembl/mnuhn/postgap/databases/'
+		pickle_file = 'lib/t/test-data/gwas_snps.merged_preclusters.pickle'
+		
+		pickle_fh = open(pickle_file, 'rb')
+		
+		import pickle
+		clusters = pickle.load(pickle_fh)
+
+		print type(clusters)
+		print "Got %s clusters." % len(clusters)
+
+		for x in range(len(clusters)):
+			print "Cluster %s has %s members." % ( x, len(clusters[x].ld_snps) )
+
+		import finemap.PostgapIntegation
+		gwas_clusters_with_posteriors = finemap.PostgapIntegation.process_GWAS_Clusters(clusters)
+		print "Got %i gwas clusters with posteriors." % len(gwas_clusters_with_posteriors)
+
+
 if __name__ == '__main__':
 	unittest.main()
