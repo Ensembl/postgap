555e01f8b4075103d6aaee791c8b906ca2517fcc
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-06-08 15:55:01 +0100

    Switched from printing to logging

--- lib/t/foo.py
+++ lib/t/foo.py
@@ -5,13 +5,18 @@
 # python -m unittest discover -s lib/t/ -p "*.py"
 
 import unittest
+import logging
+import logging.config
 
 class Blabladiblupp(unittest.TestCase):
  
 	def setUp(self):
+		logging.config.fileConfig('configuration/logging.conf')
 		pass
  
 	def test_foooooo(self):
+		
+		logger = logging.getLogger(__name__)
 		
 		import postgap.Globals
 		postgap.Globals.DATABASES_DIR = '/nfs/nobackup/ensembl/mnuhn/postgap/databases/'
@@ -22,15 +27,15 @@ class Blabladiblupp(unittest.TestCase):
 		import pickle
 		clusters = pickle.load(pickle_fh)
 
-		print type(clusters)
-		print "Got %s clusters." % len(clusters)
+		logger.info(type(clusters))
+		logger.info("Got %s clusters." % len(clusters))
 
 		for x in range(len(clusters)):
-			print "Cluster %s has %s members." % ( x, len(clusters[x].ld_snps) )
+			logger.info("Cluster %s has %s members." % ( x, len(clusters[x].ld_snps) ))
 
 		import finemap.PostgapIntegation
 		gwas_clusters_with_posteriors = finemap.PostgapIntegation.process_GWAS_Clusters(clusters)
-		print "Got %i gwas clusters with posteriors." % len(gwas_clusters_with_posteriors)
+		logger.info("Got %i gwas clusters with posteriors." % len(gwas_clusters_with_posteriors))
 
 
 if __name__ == '__main__':
