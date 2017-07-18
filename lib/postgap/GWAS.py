cd6d6374bcad169cb3919156aeea4ad2434fc85c
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-07-18 15:10:16 +0100

    When creating a new GWAS_Association, check, if the risk alleles are present in the reference and set flag accordingly. A few other changes too.

--- lib/postgap/GWAS.py
+++ lib/postgap/GWAS.py
@@ -82,17 +82,61 @@ class GWASCatalog(GWAS_source):
 	def query(self, efo):
 		logger = logging.getLogger(__name__)
 		logger.info("Querying GWAS catalog for " + efo);
-
+		
+		# E.g.: http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/search/findByUri?uri=http://www.ebi.ac.uk/efo/EFO_0000400
+		#
+		# EFO_0000400 stands for diabetes mellitus
+		#
 		server = 'http://wwwdev.ebi.ac.uk'
 		url = '/gwas/beta/rest/api/efoTraits/search/findByUri?uri=%s' % (efo)
-		
-		#print "Querying: " + server + url;
 
+		import postgap.REST
 		hash = postgap.REST.get(server, url)
+
+		'''
+			hash looks like this:
+			
+			{
+				"_embedded": {
+					"efoTraits": [
+						{
+							"trait": "diabetes mellitus",
+							"uri": "http://www.ebi.ac.uk/efo/EFO_0000400",
+							"_links": {
+								"self": {
+									"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/71"
+								},
+								"efoTrait": {
+									"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/71"
+								},
+								"studies": {
+									"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/71/studies"
+								},
+								"associations": {
+									"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/71/associations"
+								}
+							}
+						}
+					]
+				},
+				"_links": {
+					"self": {
+						"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/search/findByUri?uri=http://www.ebi.ac.uk/efo/EFO_0000400"
+					}
+				},
+				"page": {
+					"size": 20,
+					"totalElements": 1,
+					"totalPages": 1,
+					"number": 0
+				}
+			}
+		'''
+
 		list_of_GWAS_Associations = []
 
 		efoTraits = hash["_embedded"]["efoTraits"]
-
+		
 		for efoTraitHash in efoTraits:
 
 			efoTraitLinks = efoTraitHash["_links"]
@@ -103,46 +147,71 @@ class GWASCatalog(GWAS_source):
 			association_rest_response = efoTraitLinks["associations"]
 			association_url = association_rest_response["href"]
 			try:
+				# e.g.: http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/efoTraits/71/associations
+				#
 				association_response = postgap.REST.get(association_url, "")
 			except:
 				continue
-			"""
-			Example response:
-			{
-				_embedded: {
-					associations: [
-							{
-								riskFrequency: "0.05",
-								pvalueDescription: "(Triglyceride, sum)",
-								pvalueMantissa: 4,
-								pvalueExponent: -6,
-								multiSnpHaplotype: false,
-								snpInteraction: false,
-								snpType: "novel",
-								standardError: null,
-								range: null,
-								description: null,
-								orPerCopyNum: null,
-								betaNum: null,
-								betaUnit: null,
-								betaDirection: null,
-								lastMappingDate: "2016-12-25T03:48:35.000+0000",
-								lastUpdateDate: null,
-								pvalue: 0.000004,
-								_links: {}
-							}
-						]
-				},
-				_links: {}
-			}
-			"""
+			
 			associations = association_response["_embedded"]["associations"]
+			
+			'''
+				associations has this structure:
+				
+				[
 
+					{
+						"riskFrequency": "NR",
+						"pvalueDescription": null,
+						"pvalueMantissa": 2,
+						"pvalueExponent": -8,
+						"multiSnpHaplotype": false,
+						"snpInteraction": false,
+						"snpType": "known",
+						"standardError": 0.0048,
+						"range": "[NR]",
+						"description": null,
+						"orPerCopyNum": null,
+						"betaNum": 0.0266,
+						"betaUnit": "unit",
+						"betaDirection": "increase",
+						"lastMappingDate": "2016-12-24T07:36:49.000+0000",
+						"lastUpdateDate": "2016-11-25T14:37:53.000+0000",
+						"pvalue": 2.0E-8,
+						"_links": {
+							"self": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018"
+							},
+							"association": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018"
+							},
+							"study": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/study"
+							},
+							"snps": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/snps"
+							},
+							"loci": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/loci"
+							},
+							"efoTraits": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/efoTraits"
+							},
+							"genes": {
+								"href": "http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/genes"
+							}
+						}
+					},
+				...
+				]
+			'''
 			logger.info("Received " + str(len(associations)) + " associations with SNPs.")
 			logger.info("Fetching SNPs and pvalues.")
 
 			for current_association in associations:
 
+				# e.g. snp_url can be: http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/associations/16513018/snps
+				#
 				snp_url = current_association["_links"]["snps"]["href"]
 				snp_response = postgap.REST.get(snp_url, "")
 				"""
@@ -234,6 +303,39 @@ class GWASCatalog(GWAS_source):
 						continue
 
 					logger.debug("    received association with snp rsId: " + '{:12}'.format(current_snp["rsId"]) + " with a pvalue of " + str(current_association["pvalue"]))
+					
+					risk_alleles_href = current_snp["_links"]["riskAlleles"]["href"]
+					import postgap.REST
+					hash = postgap.REST.get(risk_alleles_href, ext="")
+					riskAlleles = hash["_embedded"]["riskAlleles"]
+					
+					from methods.GWAS_Lead_Snp_Orientation                 \
+					import                                                 \
+					gwas_risk_alleles_present_in_reference,                \
+					none_of_the_risk_alleles_is_a_substitution_exception,  \
+					variant_mapping_is_ambiguous_exception,                \
+					some_alleles_present_others_not_exception
+					
+					try:
+					
+						if gwas_risk_alleles_present_in_reference(riskAlleles):
+							risk_alleles_present_in_reference = True
+							logging.info("Risk allele is present in reference");
+						else:
+							risk_alleles_present_in_reference = False
+							logging.info("Risk allele is not present in reference");
+					
+					except none_of_the_risk_alleles_is_a_substitution_exception as e:
+						logger.info(str(e))
+						continue
+					
+					except variant_mapping_is_ambiguous_exception:
+						logger.info("The variant mapping is ambiguous.")
+						continue
+					
+					except some_alleles_present_others_not_exception as e:
+						logger.info(str(e));
+						continue
 
 					list_of_GWAS_Associations.append(
 						GWAS_Association(
@@ -247,6 +349,11 @@ class GWASCatalog(GWAS_source):
 							sample_size = sample_size,
 							source  = 'GWAS Catalog',
 							study   = 'PMID' + pubmedId,
+							
+							# For fetching additional information like risk allele later, if needed.
+							# E.g.: http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/singleNucleotidePolymorphisms/9765
+							rest_hash = current_snp,
+							risk_alleles_present_in_reference = risk_alleles_present_in_reference,
 							
 							odds_ratio                 = current_association["orPerCopyNum"],
 
@@ -396,9 +503,29 @@ class GRASP(GWAS_source):
 					beta_coefficient = None,
 					beta_coefficient_unit = None,
 					beta_coefficient_direction = None
+					rest_hash = None,
+					risk_alleles_present_in_reference = None,
 				)
 			except:
 				return None
+
+		if items[12] in diseases:
+			iri = items[70].split(',')[0]
+			return GWAS_Association(
+				pvalue = float(items[10]),
+				snp = "rs" + items[4],
+				disease = Disease(name = postgap.EFO.term(iri), efo = iri),
+				reported_trait = items[12].decode('latin1'),
+				source = self.display_name,
+				study = items[7],
+				sample_size = int(items[24]),
+				odds_ratio = None,
+				beta_coefficient = None,
+				beta_coefficient_unit = None,
+				beta_coefficient_direction = None
+				rest_hash = None,
+				risk_alleles_present_in_reference = None,
+			)
 
 		return None
 
@@ -455,6 +582,7 @@ class Phewas_Catalog(GWAS_source):
 					beta_coefficient = None,
 					beta_coefficient_unit = None,
 					beta_coefficient_direction = None
+					rest_hash = None
 				)
 
 		if items[2] in diseases: 
@@ -471,7 +599,8 @@ class Phewas_Catalog(GWAS_source):
 				study = None,
 				beta_coefficient = None,
 				beta_coefficient_unit = None,
-				beta_coefficient_direction = None
+				beta_coefficient_direction = None,
+				rest_hash = None
 			)
 
 		return None
@@ -527,6 +656,8 @@ class GWAS_DB(GWAS_source):
 					beta_coefficient = None,
 					beta_coefficient_unit = None,
 					beta_coefficient_direction = None
+					rest_hash = None,
+					risk_alleles_present_in_reference = None,
 				)
 
 		if items[5] in diseases:
@@ -543,6 +674,8 @@ class GWAS_DB(GWAS_source):
 				beta_coefficient = None,
 				beta_coefficient_unit = None,
 				beta_coefficient_direction = None
+				rest_hash = None,
+				risk_alleles_present_in_reference = None,
 			)
 
 		return None
