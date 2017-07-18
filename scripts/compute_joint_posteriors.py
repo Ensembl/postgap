b2ed8f5ddefa3adea31b4984f4044f4407f26c36
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-07-18 15:18:37 +0100

    Minor

--- scripts/compute_joint_posteriors.py
+++ scripts/compute_joint_posteriors.py
@@ -45,11 +45,8 @@ python scripts/compute_joint_posteriors.py \
     --output_joint_posteriors finemap/EFO_0000203/join_posteriors/eqtl_snps_linked_to_ENSG00000168038_in_Whole_Blood__gwas_cluster_0.joint_posteriors.pickle
 
 	'''
-	
-	import postgap.Globals
-	postgap.Globals.DATABASES_DIR = '/nfs/nobackup/ensembl/mnuhn/postgap/databases/'
 
-	parser = argparse.ArgumentParser(description='Run finemap')
+	parser = argparse.ArgumentParser(description='Compute joint posteriors')
 	parser.add_argument('--gwas_posterior')
 	parser.add_argument('--eqtl_posterior')
 	parser.add_argument('--output_joint_posteriors')
@@ -72,8 +69,12 @@ python scripts/compute_joint_posteriors.py \
 	pickle_fh       = open(gwas_posterior, 'rb')
 	gwas_posteriors = pickle.load(pickle_fh)
 	
+	logging.info("Gwas posteriors: " +  str(gwas_posteriors))
+	logging.info("Eqtl posteriors: " +  str(eqtl_posteriors))
+	
 	logging.info("Computing joint posteriors")
-	fm2d_sss = gwas_posteriors.joint_posterior(eqtl_posteriors)
+	twoDConfigurationSample = gwas_posteriors.joint_posterior(eqtl_posteriors)
+	logging.info("Done.")
 	
 	import os
 	output_joint_posteriors_directory = os.path.dirname(output_joint_posteriors)
@@ -82,26 +83,19 @@ python scripts/compute_joint_posteriors.py \
 		os.makedirs(output_joint_posteriors_directory)
 
 	pickle_fh = open(output_joint_posteriors, 'w')
-	pickle.dump(fm2d_sss, pickle_fh)
+	pickle.dump(twoDConfigurationSample, pickle_fh)
 	pickle_fh.close
 	
-	coloc_evidence = fm2d_sss[0]
-	res_final      = fm2d_sss[1]
+	configurations = twoDConfigurationSample.configurations
+	posterior      = twoDConfigurationSample.posterior
 	
-	configurations = res_final.configurations
-	posterior      = res_final.posterior
-
-	from pprint import pformat
-
-	logging.info("coloc_evidence: %s"     % coloc_evidence)
-	logging.info("Got %s configurations." % len(configurations))
-	logging.info("configurations: "       + pformat(configurations))
-	logging.info("Got %s posteriors: "    % len(posterior))
-	logging.info("posterior: "            + pformat(posterior))
-
+	logging.info("Joint posteriors: " + str(twoDConfigurationSample));
 	logging.info("Posteriors have been written to " + output_joint_posteriors)
 	logging.info("All done.");
 
 if __name__ == "__main__":
 	main()
+
+
+
 
