6e3045e56bffb5c4e1bb8b6f2e8842bf1974ba84
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-08-02 09:57:30 +0100

    Made gwas pvalue cutoff a global variable to avoid unnecessary work when using the large gwas pvalue files.

--- lib/postgap/Globals.py
+++ lib/postgap/Globals.py
@@ -52,3 +52,5 @@ work_directory = "notset"
 
 finemap_gwas_clusters_directory = None
 finemap_eqtl_clusters_directory = None
+
+GWAS_PVALUE_CUTOFF = 1e-4
--- lib/postgap/Integration.py
+++ lib/postgap/Integration.py
@@ -44,7 +44,6 @@ from postgap.Utils import *
 import logging
 
 phenotype_cache = ()
-PVALUE_CUTOFF = 1e-4
 MAX_GWAS_HITS = 500
 
 def diseases_to_genes(diseases, efos, populations, tissues):
@@ -73,9 +72,11 @@ def diseases_to_gwas_snps(diseases, efos):
 	"""
 	logger = logging.getLogger(__name__)
 	
-	res = filter(lambda X: X.pvalue < PVALUE_CUTOFF, scan_disease_databases(diseases, efos))
+	from postgap.Globals import GWAS_PVALUE_CUTOFF
+	
+	res = filter(lambda X: X.pvalue < GWAS_PVALUE_CUTOFF, scan_disease_databases(diseases, efos))
 
-	logger.info("Found %i GWAS SNPs associated to diseases (%s) or EFO IDs (%s) after p-value filter (%f)" % (len(res), ", ".join(diseases), ", ".join(efos), PVALUE_CUTOFF))
+	logger.info("Found %i GWAS SNPs associated to diseases (%s) or EFO IDs (%s) after p-value filter (%f)" % (len(res), ", ".join(diseases), ", ".join(efos), GWAS_PVALUE_CUTOFF))
 
 	# Only return the top hits
 	return sorted(res, key = lambda gwas_snp: gwas_snp.pvalue)[:MAX_GWAS_HITS]
