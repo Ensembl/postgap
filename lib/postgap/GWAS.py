2700f90e1dbb67877c52e061b206c1643fd6cd85
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-10-17 15:41:06 +0100

    Updates to reflect the changes in the GWAS Rest API

--- lib/postgap/GWAS.py
+++ lib/postgap/GWAS.py
@@ -84,9 +84,9 @@ class GWASCatalog(GWAS_source):
 		#
 		# EFO_0000400 stands for diabetes mellitus
 		#
-		server = 'http://wwwdev.ebi.ac.uk'
-		url = '/gwas/beta/rest/api/efoTraits/search/findByUri?uri=%s' % (efo)
-
+		server = 'http://www.ebi.ac.uk'
+		url = '/gwas/beta/rest/api/efoTraits/search/findByEfoUri?uri=%s' % (efo)
+		
 		hash = postgap.REST.get(server, url)
 
 		'''
@@ -260,20 +260,8 @@ class GWASCatalog(GWAS_source):
 				}
 				"""
 				pubmedId = study_response["pubmedId"]
-
-				diseaseTrait_url = study_response["_links"]["diseaseTrait"]["href"]
-				diseaseTrait_response = postgap.REST.get(diseaseTrait_url, "")
-				"""
-				Example response:
-				{
-					trait: "Response to statin therapy",
-					_links: {}
-				}
-				"""
-				diseaseTrait = diseaseTrait_response['trait']
-
-				ancestries_url = study_response["_links"]["ancestries"]["href"]
-				ancestries_response = postgap.REST.get(ancestries_url, "")
+				diseaseTrait = study_response["diseaseTrait"]["trait"]
+				ancestries = study_response["ancestries"]
 				"""
 				Example response:
 				{
@@ -292,7 +280,7 @@ class GWASCatalog(GWAS_source):
 						_links: {}
 				}
 				"""
-				sample_size = sum(int(ancestry['numberOfIndividuals']) for ancestry in ancestries_response['_embedded']['ancestries'] if ancestry['numberOfIndividuals'] is not None)
+				sample_size = sum(int(ancestry['numberOfIndividuals']) for ancestry in ancestries if ancestry['numberOfIndividuals'] is not None)
 
 				for current_snp in singleNucleotidePolymorphisms:
 					
