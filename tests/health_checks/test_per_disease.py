5a8b4766601c0524db66ff57142f21b9bf86371f
peatroot <garethpeat@gmail.com>
2018-01-24 22:35:26 +0000

    Rename test

--- tests/health_checks/test_per_disease.py
+++ tests/health_checks/test_per_disease.py
@@ -11,7 +11,7 @@ class TestPostgapPerDisease(TestPostgapBase):
     def setUp(self):
         self.per_disease = self.pg.groupby('disease_efo_id')
 
-    def test_disease_efo_and_name_combination_unique(self):
+    def test_each_disease_efo_id_has_unique_disease_name(self):
         self.assert_groupby_series_is_unique_per_group(
             self.per_disease.disease_name
         )
