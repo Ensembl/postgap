d92ec23894180a108b0c1c814e305ec7bb00a76a
peatroot <garethpeat@gmail.com>
2018-01-24 23:01:01 +0000

    Add test_per_ld_snp.py

---
+++ tests/health_checks/test_per_ld_snp.py
@@ -0,0 +1,26 @@
+# ------------------------------------------------
+# built-ins
+import unittest
+
+# local
+from .base import TestPostgapBase
+# ------------------------------------------------
+
+class TestPostgapPerLdSnp(TestPostgapBase):
+
+    def setUp(self):
+        self.per_ld_snp = self.pg.groupby('ld_snp_rsID')
+
+    def test_each_ld_snp_rsID_has_unique_chrom(self):
+        self.assert_groupby_series_is_unique_per_group(
+            self.per_ld_snp.chrom
+        )
+
+    def test_each_ld_snp_rsID_has_unique_pos(self):
+        self.assert_groupby_series_is_unique_per_group(
+            self.per_ld_snp.pos
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
