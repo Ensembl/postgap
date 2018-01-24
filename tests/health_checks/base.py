8418301ccbb33b2aeff386170e8975d396aa1d28
peatroot <garethpeat@gmail.com>
2018-01-24 21:56:55 +0000

    Add more row tests for column format checks

--- tests/health_checks/base.py
+++ tests/health_checks/base.py
@@ -1,4 +1,12 @@
+# ------------------------------------------------
+# built-ins
 import unittest
+# ------------------------------------------------
+
+VALID_CHROMOSOMES = [*[str(chr) for chr in range(23)], 'X', 'Y']
+VALID_GWAS_SOURCES = ['GWAS Catalog']
+VALID_SNP_ID_REGEX = '^rs\d+$'
+
 
 class TestPostgapBase(unittest.TestCase):
     """
@@ -12,10 +20,37 @@ class TestPostgapBase(unittest.TestCase):
     def assert_series_in_range(self, series, low, high):
         """
         Check if all values in a `pandas.Series` are in the range [low, high].
-
-        If any values fail the condition, the first exception is printed.
         """
         between = series.between(low, high)
         all_between = between.all()
         self.assertTrue(all_between,
                         series[~between].head(1).to_string(index=False))
+
+    def assert_valid_snp_id(self, series):
+        """
+        Check if all values in a `pandas.Series` are valid SNP ids.
+        """
+        valid_snp_id = series.str.match(VALID_SNP_ID_REGEX)
+        all_valid_snp_id = valid_snp_id.all()
+        self.assertTrue(all_valid_snp_id,
+                        series[~valid_snp_id].head(1).to_string(index=False))
+
+    def assert_series_valid_chrom(self, series):
+        """
+        Check if all values in a `pandas.Series` are valid chromosomes.
+        """
+        series_str = series.apply(str)
+        chrs = series_str.unique()
+        all_valid = all(chr in VALID_CHROMOSOMES for chr in chrs)
+        invalid_freqs = series_str[~series_str.isin(VALID_CHROMOSOMES)].value_counts()
+        self.assertTrue(all_valid, invalid_freqs)
+
+    def assert_series_valid_gwas_source(self, series):
+        """
+        Check if all values in a `pandas.Series` are valid GWAS sources.
+        """
+        sources = series.unique()
+        all_valid = all(s in VALID_GWAS_SOURCES for s in sources)
+        invalid_freqs = series[~series.isin(VALID_GWAS_SOURCES)].value_counts()
+        self.assertTrue(all_valid, invalid_freqs)
+        
--- tests/health_checks/test_row.py
+++ tests/health_checks/test_row.py
@@ -13,14 +13,114 @@ class TestPostgapRow(TestPostgapBase):
     def test_canary(self):
         self.assertTrue(True)
 
-    def test_score_range(self):
-        self.assert_series_in_range(self.pg.score, 0, 6)
+    # ld_snp_rsID
+    def test_col_format_ld_snp_rsID(self):
+        self.assert_valid_snp_id(self.pg.ld_snp_rsID)
 
-    def test_r2_range(self):
+    # chrom
+    def test_col_format_chrom(self):
+        self.assert_series_valid_chrom(self.pg.chrom)
+
+    # pos
+
+    # GRCh38_chrom
+    def test_col_format_GRCh38_chrom(self):
+        self.assert_series_valid_chrom(self.pg.GRCh38_chrom)
+
+    # GRCh38_pos
+
+    # afr_maf
+    def test_col_format_afr_maf(self):
+        self.assert_series_in_range(self.pg.afr_maf, 0.0, 1.0)
+
+    # amr_maf
+    def test_col_format_amr_maf(self):
+        self.assert_series_in_range(self.pg.amr_maf, 0.0, 1.0)
+
+    # eas_maf
+    def test_col_format_eas_maf(self):
+        self.assert_series_in_range(self.pg.eas_maf, 0.0, 1.0)
+
+    # eur_maf
+    def test_col_format_eur_maf(self):
+        self.assert_series_in_range(self.pg.eur_maf, 0.0, 1.0)
+
+    # sas_maf
+    def test_col_format_sas_maf(self):
+        self.assert_series_in_range(self.pg.sas_maf, 0.0, 1.0)
+
+    # gene_symbol
+    # gene_id
+
+    # gene_chrom
+    def test_col_format_gene_chrom(self):
+        self.assert_series_valid_chrom(self.pg.gene_chrom)
+
+    # gene_tss
+
+    # GRCh38_gene_chrom
+    def test_col_format_GRCh38_gene_chrom(self):
+        self.assert_series_valid_chrom(self.pg.GRCh38_gene_chrom)
+
+    # GRCh38_gene_pos
+    # disease_name
+    # disease_efo_id
+    # score
+    # rank
+
+    # r2
+    def test_col_format_r2(self):
         self.assert_series_in_range(self.pg.r2, 0.7, 1.0)
 
-    def test_gwas_pval_range(self):
+    # cluster_id
+    
+    # gwas_source
+    def test_col_format_gwas_source(self):
+        self.assert_series_valid_gwas_source(self.pg.gwas_source)
+
+    # gwas_snp
+    def test_col_format_gwas_snp(self):
+        self.assert_valid_snp_id(self.pg.gwas_snp)
+
+    # gwas_pvalue
+    def test_col_format_gwas_pval(self):
         self.assert_series_in_range(self.pg.gwas_pvalue, 0.0, 1.0)
+
+    # gwas_pvalue_description
+    # gwas_odds_ratio
+    # gwas_beta
+    # gwas_size
+    # gwas_pmid
+    # gwas_study
+    # gwas_reported_trait
+    # ls_snp_is_gwas_snp
+    # vep_terms
+    # vep_sum
+    # vep_mean
+
+    # GTEx
+    def test_col_format_GTEx(self):
+        self.assert_series_in_range(self.pg.GTEx, 0.0, 1.0)
+
+    # VEP
+    # Fantom5
+    def test_col_format_Fantom5(self):
+        self.assert_series_in_range(self.pg.Fantom5, 0.0, 1.0)
+
+    # DHS
+    def test_col_format_DHS(self):
+        self.assert_series_in_range(self.pg.DHS, 0.0, 1.0)
+
+    # PCHiC
+    def test_col_format_PCHiC(self):
+        self.assert_series_in_range(self.pg.PCHiC, 0.0, 1.0)
+
+    # Nearest
+    # Regulome
+    # VEP_reg
+    # vep_max_score
+    # fg_score
+    # snp_gene_dist
 
 
 if __name__ == '__main__':
