016d5208f4ca4548e43881d4a4d2a599d3fa6f34
peatroot <garethpeat@gmail.com>
2018-02-05 18:42:20 +0000

    Lowercase and

--- tests/data_checks/test_pvalue_filtering.py
+++ tests/data_checks/test_pvalue_filtering.py
@@ -8,7 +8,7 @@ from utils.base import TestPostgapBase
 
 class TestPValueFiltering(TestPostgapBase):
 
-    def test_one_pvalue_per_gwas_pmid_AND_efo_id(self):
+    def test_one_pvalue_per_gwas_pmid_and_efo_id(self):
         self.skipTest('ONE PVALUE PER GWAS PMID AND EFO ID')
 
     def test_largest_pvalue_of_set_per_gwas_pmid_AND_efo_id(self):
