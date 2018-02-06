30ebeb89ed0f55e7634698f1d1ee894b775c161f
Daniel Zerbino <zerbino@ebi.ac.uk>
2018-02-06 19:10:23 +0000

    Bug in region filter, closes #117

--- lib/postgap/RegionFilter.py
+++ lib/postgap/RegionFilter.py
@@ -5,10 +5,10 @@ def region_filter(clusters):
 	return filter(lambda cluster: not cluster_overlap_regions(cluster, BLACKLISTED_REGIONS), clusters)
 
 def cluster_overlap_regions(cluster, regions):
-	return not any(cluster_overlap_region(cluster, region) for region in regions)
+	return any(cluster_overlap_region(cluster, region) for region in regions)
 
 def cluster_overlap_region(cluster, region):
-	return not any(snp_overlap_region(snp, region) for snp in cluster.ld_snps)
+	return any(snp_overlap_region(snp, region) for snp in cluster.ld_snps)
 
 def snp_overlap_region(snp, region):
 	return snp.chrom == region.chrom and snp.pos >= region.start and snp.pos < region.end
