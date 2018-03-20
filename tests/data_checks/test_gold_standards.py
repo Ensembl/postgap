97950f8003ad62f76464cc555784dc53666825d4
wnewell <newell.wr@gmail.com>
2018-03-20 09:47:52 +0000

    Gold Standard Genetic Examples Added EXPECTED_ASSIGNMENTS = dict(SNP, [Genes]) extracted by qaqc.GoldStandardsExamples.py

--- tests/data_checks/test_gold_standards.py
+++ tests/data_checks/test_gold_standards.py
@@ -3,7 +3,8 @@
 import unittest
 
 # local
-from utils.base import TestPostgapBase
+from tests.utils.base import TestPostgapBase
+
 # ------------------------------------------------
 
 # TODO: Expected (target, disease) pairs from experts
@@ -12,8 +13,32 @@ EXPECTED_ASSOCIATIONS = [
     ('ENSG00002', 'EFO_00001')
 ]
 
-class TestPostgapGoldStandards(TestPostgapBase):
+# TODO: Expected (snp, gene) pairs from experts
+# extracted by qaqc.GoldStandardExamples.py
+EXPECTED_ASSIGNMENTS = {
+    'rs10872142': {'FRK'},
+    'rs10889356': {'DOCK7, ANGPTL3'},
+    'rs10941679': {'MRPS30', 'FGF10'},
+    'rs111961716': {'ABHD8', 'ANKLE1'},
+    'rs12740374': {'SORT1'},
+    'rs1421085': {'ARID5B', 'IRX3', 'IRX5'},
+    'rs1427407': {'BCL11A'},
+    'rs2277862': {'CPNE1'},
+    'rs356168': {'SNCA'},
+    'rs378854': {'MYC', 'PVT1'},
+    'rs4442975': {'IGFBP5'},
+    'rs4500960': {'TBR1'},
+    'rs554219': {'CCND1'},
+    'rs56069439': {'ABHD8', 'ANKLE1'},
+    'rs7463708': {'PCAT1'},
+    'rs75915166': {'CCND1'},
+    'rs7606173': {'BCL11A'},
+    'rs78540526': {'CCND1'},
+    'rs9926296': {'EDN1'}
+}
 
+
+class TestPostgapGoldStandards(TestPostgapBase):
     def test_each_expected_association_is_present(self):
         self.skipTest('CHECK ALL GOLD STANDARD EXPECTED ASSOCIATIONS')
 
