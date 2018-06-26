861e456976c19fc6d2920c70e36385f8da6920c0
Daniel Zerbino <zerbino@ebi.ac.uk>
2018-06-26 19:45:10 +0100

    QC typos

--- lib/postgap/FinemapIntegration.py
+++ lib/postgap/FinemapIntegration.py
@@ -44,11 +44,11 @@ def finemap_gwas_cluster(cluster, populations):
 
 	'''
 	if len(cluster.ld_snps) == len(cluster.gwas_snps):
-		ld_snps, ld_matrix, z_scores = compute_ld_matrix(cluster)
+		ld_snps, ld_matrix, z_scores, betas = compute_ld_matrix(cluster)
 	elif postgap.Globals.GWAS_SUMMARY_STATS_FILE is not None:
-		ld_snps, ld_matrix, z_scores = extract_z_scores_from_file(cluster)
+		ld_snps, ld_matrix, z_scores, betas = extract_z_scores_from_file(cluster)
 	else:
-		ld_snps, ld_matrix, z_scores = impute_z_scores(cluster)
+		ld_snps, ld_matrix, z_scores = impute_z_scores(cluster) # TODO: Betas -> waiting for formula
 
 	## Define experiment label (serves for debugging logs)
 	chrom = ld_snps[0].chrom
@@ -64,12 +64,13 @@ def finemap_gwas_cluster(cluster, populations):
 
 	## Compute posteriors
 	configuration_posteriors = postgap.Finemap.finemap(
-		labels       = ld_snp_ids,
-		z_scores     = z_scores,
+		z_scores     = numpy.array(z_scores),
+		beta_scores  = numpy.array(betas),
 		cov_matrix   = ld_matrix,
 		n            = sample_size,
- 		kmax         = 1,
-		sample_label = sample_label
+		labels       = ld_snp_ids,
+		sample_label = sample_label,
+ 		kmax         = 1
 	)
 
 	assert len(ld_snps) ==  ld_matrix.shape[0]
@@ -92,15 +93,18 @@ def compute_ld_matrix(cluster):
 
 	# Aggregate z_scores into a single vector
 	z_scores = []
+	betas = []
 	for i in cluster.ld_snps:
 		z_scores.append(0)
+		betas.append(0)
 	gwas_snp_hash = dict((gwas_snp.snp.rsID, gwas_snp) for gwas_snp in cluster.gwas_snps)
 	for index, ld_snp in enumerate(ld_snps):
 		z_scores[index] = gwas_snp_hash[ld_snp.rsID].z_score
+		betas[index] = gwas_snp_hash[ld_snp.rsID].beta
 
 	assert len(ld_snps) ==  ld_matrix.shape[0]
 	assert len(ld_snps) ==  ld_matrix.shape[1]
-	return ld_snps, ld_matrix, z_scores
+	return ld_snps, ld_matrix, z_scores, betas
 
 def extract_z_scores_from_file(cluster):
 	'''
@@ -133,10 +137,11 @@ def extract_z_scores_from_file(cluster):
 	## Update list of LD SNPs
 	ld_snps = [ld_snp_hash[rsID] for rsID in ld_snp_ids]
 	z_scores = [z_score_from_pvalue(ld_snp_results[rsID][0], ld_snp_results[rsID][1]) for rsID in ld_snp_ids]
+	betas = [ld_snp_results[rsID][1] for rsID in ld_snp_ids]
 
 	assert len(ld_snps) ==  ld_matrix.shape[0]
 	assert len(ld_snps) ==  ld_matrix.shape[1]
-	return ld_snps, ld_matrix, z_scores
+	return ld_snps, ld_matrix, z_scores, betas
 
 def impute_z_scores(cluster):
 	'''
@@ -203,7 +208,7 @@ def compute_gene_joint_posterior(cluster, gene, tissue_snp_eQTL_hash):
 		Compute collocation posterior of gene expression and GWAS phenotype at the specified cluster and tissue
 		Arg1: GWAS_Cluster
 		Arg2: Gene
-		Arg4: Hash of hashes: Tissue => SNP => Float
+		Arg4: Hash of hashes: Tissue => SNP => (Float, Float)
 		Returntype: Hash of hashes: Tissue => Float
 	"""
 	assert len(cluster.ld_snps) == cluster.ld_matrix.shape[0], (len(cluster.ld_snps), cluster.ld_matrix.shape[0], cluster.ld_matrix.shape[1])
@@ -216,14 +221,15 @@ def compute_gene_tissue_joint_posterior(cluster, tissue, gene, eQTL_snp_hash):
 		Arg1: GWAS_Cluser
 		Arg2: Tissue (string)
 		Arg3: Gene
-		Arg4: Hash string (rsID) => float (z-score)
+		Arg4: Hash string (rsID) => (Float (z-score), Float (beta))
 		Returntype: Float
 	"""
 	assert len(cluster.ld_snps) == cluster.ld_matrix.shape[0], (len(cluster.ld_snps), cluster.ld_matrix.shape[0], cluster.ld_matrix.shape[1])
 	assert len(cluster.ld_snps) == cluster.ld_matrix.shape[1], (len(cluster.ld_snps), cluster.ld_matrix.shape[0], cluster.ld_matrix.shape[1])
 	## Determine which SNPs are missing values
 	missing_indices = numpy.array([index for index, ld_snp in enumerate(cluster.ld_snps) if ld_snp.rsID not in eQTL_snp_hash]).astype(int)
-	known_z_scores = numpy.array([eQTL_snp_hash[ld_snp.rsID] for ld_snp in cluster.ld_snps if ld_snp.rsID in eQTL_snp_hash])
+	known_z_scores = numpy.array([eQTL_snp_hash[ld_snp.rsID][0] for ld_snp in cluster.ld_snps if ld_snp.rsID in eQTL_snp_hash])
+	known_betas = numpy.array([eQTL_snp_hash[ld_snp.rsID][1] for ld_snp in cluster.ld_snps if ld_snp.rsID in eQTL_snp_hash])
 	assert len(known_z_scores) > 0
 	assert len(missing_indices) != len(cluster.ld_snps), (missing_indices, known_z_scores)
 	assert len(cluster.ld_snps) == cluster.ld_matrix.shape[0], (len(cluster.ld_snps), cluster.ld_matrix.shape[0], cluster.ld_matrix.shape[1])
@@ -248,16 +254,20 @@ def compute_gene_tissue_joint_posterior(cluster, tissue, gene, eQTL_snp_hash):
 
 		# Aggregate z_scores into a single vector
 		z_scores = []
+		betas = []
 		for i in cluster.ld_snps:
 			z_scores.append(0)
+			betas.append(0)
 		for index, z_score in zip(missing_indices, z_shrink_imputed):
 			z_scores[index] = z_score
+			# TODO Betas -> waiting for formula?
 		for index, ld_snp in enumerate(cluster.ld_snps):
 			if ld_snp.rsID in eQTL_snp_hash:
-				z_scores[index] = eQTL_snp_hash[ld_snp.rsID]
+				z_scores[index] = eQTL_snp_hash[ld_snp.rsID][0]
+				betas[index] = eQTL_snp_hash[ld_snp.rsID][1]
 	else:
 		z_scores = known_z_scores
-			
+		betas = known_betas
 
 	## Define experiment label (serves for debugging logs)
 	chrom = cluster.ld_snps[0].chrom
@@ -267,12 +277,13 @@ def compute_gene_tissue_joint_posterior(cluster, tissue, gene, eQTL_snp_hash):
 
 	## Compute posteriors
 	eQTL_configuration_posteriors = postgap.Finemap.finemap(
-		labels       = [ld_snp.rsID for ld_snp in cluster.ld_snps],
-		z_scores     = z_scores,
+		z_scores     = numpy.array(z_scores),
+		beta_scores  = numpy.array(betas),
 		cov_matrix   = cluster.ld_matrix,
 		n            = 500, #TODO extract eQTL sample sizes
  		kmax         = 1,
-		sample_label = sample_label
+		sample_label = sample_label,
+		labels       = [ld_snp.rsID for ld_snp in cluster.ld_snps]
 	)
 
 	## Joint posterior
@@ -290,7 +301,7 @@ def organise_eQTL_data(associations):
 	for association in associations:
 		for evidence in association.cisregulatory_evidence:
 			if evidence.source == 'GTEx':
-				res[association.gene][evidence.tissue][association.snp.rsID] = evidence.z_score
+				res[association.gene][evidence.tissue][association.snp.rsID] = (evidence.z_score, evidence.beta)
 	return res
 	
 def sign(number):
