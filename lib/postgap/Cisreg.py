e651fc14171cdc9c6ccf8bb8800fb5a972337f35
Daniel Zerbino <zerbino@ebi.ac.uk>
2018-08-17 10:50:51 +0100

    Small code simplification

--- lib/postgap/Cisreg.py
+++ lib/postgap/Cisreg.py
@@ -292,7 +292,7 @@ class GTEx(Cisreg_source):
 
 
 			res = []
-			for k in range(0, len(p_val_index[0])):
+			for k in range(len(p_val_index[0])):
 				pvalue = p_val[p_val_index[0][k]][p_val_index[1][k]][p_val_index[2][k]]
 				
 				if pvalue > 1 or pvalue < 0:
