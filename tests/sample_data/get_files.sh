abcdc82138888247f84e28f3a1ee2496a455b478
Daniel Zerbino <zerbino@ebi.ac.uk>
2019-08-16 08:58:20 +0100

    Adding download of data files

---
+++ tests/sample_data/get_files.sh
@@ -0,0 +1,2 @@
+wget https://storage.googleapis.com/postgap-data/postgap.20180817.B-cell_acute_lymphoblastic_leukemia.txt.gz
+wget https://storage.googleapis.com/postgap-data/postgap.20180817.asthma.txt.gz
