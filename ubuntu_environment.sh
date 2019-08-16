6fa5976d780251c5b3d1e5d1cdc53c351b14975d
Daniel Zerbino <zerbino@ebi.ac.uk>
2019-08-16 09:57:45 +0100

    Typo in libssl-dev dependency

---
+++ ubuntu_environment.sh
@@ -0,0 +1,7 @@
+apt install -y git gcc make g++ curl autoconf pkg-config libz-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libgsl-dev libsqlite3-dev libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 libssl-dev
+
+wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
+
+sh Anaconda2-2019.07-Linux-x86_64.sh -b -p $PWD/anaconda
+eval "$($PWD/anaconda/bin/conda shell.$0 hook)"
+conda init
