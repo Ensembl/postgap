11f476e97f662795283846cff0cf9cdbdf00d748
Daniel Zerbino <daniel.zerbino@gmail.com>
2019-08-20 13:59:34 +0100

    Adding testing dependency

--- scripts/installation/ubuntu_environment.sh
+++ scripts/installation/ubuntu_environment.sh
@@ -1,4 +1,4 @@
-apt install -y git gcc make g++ curl autoconf pkg-config libz-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libgsl-dev libsqlite3-dev libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 libssl-dev
+apt install -y git gcc make g++ curl autoconf pkg-config libz-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libgsl-dev libsqlite3-dev libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 libssl-dev virtual-env
 
 wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
 
