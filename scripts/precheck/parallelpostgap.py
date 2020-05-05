28374aefaea1a6264c3c1507989cd5286a4a7fa0
YalanBi <biyalan@yeah.net>
2020-05-05 16:45:53 +0100

    add time tracking

--- scripts/precheck/parallelpostgap.py
+++ scripts/precheck/parallelpostgap.py
@@ -6,13 +6,15 @@ This is a script file for preworks before running postgap.py in parallel
 """
 
 import argparse
+import glob
 import os
+import pandas as pd
+import shutil
 import sys
 import time
-import glob
-import pandas as pd
 
 #note: temporarily run this code under the home directory!!!!!!!!!!
+starttime = time.strftime("%y%m%d%H%M%S", time.localtime()) #eg:200501154145
 
 # get command line arguments
 parser = argparse.ArgumentParser(description='to read command line arguments')
@@ -25,34 +27,39 @@ database_dir = args.database_dir
 sumstats = args.summary_stats
 timegap = args.time_interval
 
-#1. check the filetype of the GWAS summary statistics
-filetype = sumstats.split('.')[-1] #eg:xlsx
-
-#a. if it is an excel file, convert it into a tsv file
-if filetype in ['xls', 'xlsx', 'xlsm', 'xlsb']:
-	sumstats_old = sumstats
-	sumstats = sumstats.replace(filetype, 'tsv')
-	pd.read_excel(sumstats_old).to_csv(sumstats, header=True, index=False, sep='\t')
-	#os.remove(sumstats_old)
-	print(sumstats_old.split('/')[-1] + ' is an excel file, convert it into a tab-separated file')
-
-#b. if it is ".out", change it to ".tsv". Note: conflict with the ".out" file from job summary
-if filetype == 'out':
-	sumstats_old = sumstats
-	sumstats = sumstats.replace(filetype, 'tsv')
-	os.rename(sumstats_old, sumstats)
-	print('change filename ' + sumstats_old.split('/')[-1] + ' to ' + sumstats.split('/')[-1])
-
-# get relevant info
+#0. preparations
+# get basic info about the GWAS summary statistics
 filename = sumstats.split('/')[-1]
 filedir = sumstats.replace(filename, '') if len(sumstats.split('/')) > 1 else './'
-print(filename + ' is in the ' + ('current directory' if filedir == './' else ('directory ' + filedir)))
 filetype = filename.split('.')[-1] #eg:tsv
-fnstart = filename.replace('.' + filetype, '') #eg:dubois_test
-print('start pre-checking for ' + fnstart)
+fnstart = filename.replace('.' + filetype, '') #eg:WojcikGL
+print(filename + ' is in the ' + ('current directory' if filedir == './' else ('directory ' + filedir)) + '\nstart pre-checking for ' + fnstart)
+
+# create a temporary folder for all the intermediate files that generated during the analysis
+tempdir = filedir + fnstart + '_' + starttime + '/'
+os.makedirs(tempdir)
+
+#1. check the filetype of the GWAS summary statistics
+#a. if it is an excel file, convert it into a tsv file
+if filetype in ['xls', 'xlsx', 'xlsm', 'xlsb']:
+	pd.read_excel(sumstats).to_csv(tempdir + fnstart + '.tsv', header=True, index=False, sep='\t')
+	print(filename + ' is an excel file, convert it into a tab-separated file')
+	# update filetype and filename
+	filetype = 'tsv'
+	filename = fnstart + '.' + filetype
+else:
+	#b. if it is ".out", change it to ".tsv". Note: conflict with the ".out" file from job summary
+	if filetype == 'out':
+		os.rename(sumstats, filedir + fnstart + '.tsv')
+		print('change filename from ' + filename + ' to ' + fnstart + '.tsv')
+		# update filetype and filename
+		filetype = 'tsv'
+		filename = fnstart + '.' + filetype
+
+	shutil.copyfile(filedir + filename, tempdir + filename)
 
 # save the first line into to a variable for the following checking step
-f = open(sumstats)
+f = open(tempdir + filename)
 checkline = f.readline()
 f.close()
 
@@ -69,12 +76,11 @@ else:
 			print(filename + ' is separated by ' + ('space' if s == ' ' else s) + ', and change the delimiter to tab!')
 
 			# replace delimiter with tab, and save into a new file
-			os.rename(sumstats, filedir + 'backup_' + filename)
-			os.system('cat ' + filedir + 'backup_' + filename + ' | tr "' + s + '" "\t" > ' + sumstats)
-			os.remove(filedir + 'backup_' + filename)
+			os.rename(tempdir + filename, tempdir + fnstart + '_backup.' + filetype)
+			os.system('cat ' + tempdir + fnstart + '_backup.' + filetype + ' | tr "' + s + '" "\t" > ' + tempdir + filename)
 
 			# update the checkline
-			f = open(filedir + filename)
+			f = open(tempdir + filename)
 			checkline = f.readline()
 			f.close()
 
@@ -86,7 +92,7 @@ else:
 		sys.exit('error with delimiter')
 
 # now the separator is tab for sure
-#3. check the very important three colnames: variant_id, beta, p-value
+#3. check three very important colnames: variant_id, beta, p-value
 if all([impnames in checkline.split() for impnames in ['variant_id', 'beta', 'p-value']]):
 	print('colnames checking passed')
 else:
@@ -111,13 +117,13 @@ for n in chrlist:
 	jobname = fnstart + '-chr' + n
 
 	# create a new file for each chromosome, and run postgap.py |||||| note: -m, -d, results & output2!!!!!!!!!!!
-	os.system("(head -n1 " + sumstats + " && awk 'NR==FNR {a[$3]; next} $" + ncol_rsid + " in a {print $0}' yalan/variants/variants_rsID_coords-chr" + n + ".tsv " + sumstats + ") > " + filedir + chrfile)
-	os.system("gsub -m 2 -n " + jobname + " -d " + filedir + " -q production-rh74 'python postgap/POSTGAP.py --database_dir " + database_dir + " --summary_stats " + filedir + chrfile + " --hdf5 /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5 --sqlite /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5.sqlite3 --bayesian --output " + filedir + jobname + "_res.txt --output2 " + filedir + jobname + "_op2.txt -g' -r")
+	os.system("(head -n1 " + tempdir + filename + " && awk 'NR==FNR {a[$3]; next} $" + ncol_rsid + " in a {print $0}' yalan/variants/variants_rsID_coords-chr" + n + ".tsv " + tempdir + filename + ") > " + tempdir + chrfile)
+	os.system("gsub -m 2 -n " + jobname + '_' + starttime + " -d " + tempdir + " -q production-rh74 'python postgap/POSTGAP.py --database_dir " + database_dir + " --summary_stats " + tempdir + chrfile + " --hdf5 /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5 --sqlite /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5.sqlite3 --bayesian --output " + tempdir + jobname + "_res.txt --output2 " + tempdir + jobname + "_op2.txt -g' -r")
 	
-	print('chr' + n + ' is running!')
+	print(time.strftime("%H:%M:%S", time.localtime()) + ', chr' + n + ' is submitted!')
 
 #5. wait and check if all the jobs are done
-#time.sleep(120)
+#time.sleep(60)
 print('check whether all the jobs have been done ...')
 
 jobstalist = [False] * len(chrlist)
@@ -125,32 +131,49 @@ while not all(jobstalist):
 	# check all unfinished jobs
 	for n in [i for i,s in zip(chrlist, jobstalist) if not s]:
 		jobname = fnstart + '-chr' + n
-		os.system('bjobs -J ' + jobname + ' | cat > jobstatus.txt')
+		os.system('bjobs -J ' + jobname + '_' + starttime + ' | cat > ' + tempdir + 'jobstatus.txt')
 		#jobstatus.txt is an empty file, which means job isn't running
-		jobstalist[-1 if n == chrlist[-1] else (int(n) - 1)] = True if os.path.getsize('jobstatus.txt') == 0 else False
+		jobstalist[-1 if n == chrlist[-1] else (int(n) - 1)] = True if os.path.getsize(tempdir + 'jobstatus.txt') == 0 else False
 
 	if all(jobstalist):
 		#a job isn't running could be either finished or killed
 		print('no job is running now, check if they all quitted normally')
-		os.remove('jobstatus.txt')
+		os.remove(tempdir + 'jobstatus.txt')
 		break
 	else:
-		print(', '.join([('chr' + i) for i,s in zip(chrlist, jobstalist) if not s]) + ' unfinished, wait for 30s and check again ...')
+		print('/'.join([('chr' + i) for i,s in zip(chrlist, jobstalist) if not s]) + ' unfinished, wait for 30s and check again ...')
 		time.sleep(30)
 
-#6. combine all the results into one file
-if all([os.path.getsize(f) == 0 for f in glob.glob(filedir + fnstart + '-chr*.err')]):
-	print('no error warnings, wrap all the results together')
+#6. check if any has errors, and track running/CPU time
+sumdf = pd.DataFrame(columns=['chr', 'success', 'run_time', 'cpu_time', 'max_mem', 'ave_mem'], index=range(len(chrlist)))
 
+for i,n in enumerate(chrlist):
+	jobname = fnstart + '-chr' + n
+
+	f = open(tempdir + jobname + '_' + starttime + '.out')
+	outtext = f.readlines()
+	f.close()
+
+	if 'Successfully completed' in outtext[-23]: complete = True
+	elif 'Exited with exit code' in outtext[-23]: complete = False
+	else: complete = 'N/A'
+
+	sumdf.iloc[i] = [n, complete, outtext[-11].split()[-2], outtext[-19].split()[-2], outtext[-18].split()[-2] if 'MB' in outtext[-18] else 'N/A', outtext[-17].split()[-2] if 'MB' in outtext[-17] else 'N/A']
+
+sumdf.to_csv(filedir + fnstart + '_jobsum_' + starttime + '.txt', header=True, index=False, sep='\t', na_rep='N/A')
+
+#7. combine all the results into one file
+if all(sumdf['success']):
+	print('all the jobs have been completed successfully, and wrap all the results together')
 	# results file could have only column names, and pd.concat can combine even empty dfs
-	resdf = pd.concat([pd.read_csv(f, sep='\t', header=0) for f in glob.glob(filedir + fnstart + '-chr*_res.txt')], sort=False, ignore_index=True)
-	resdf.to_csv(filedir + fnstart + '_results.txt', header=True, index=False, sep='\t', na_rep='N/A')
+	resdf = pd.concat([pd.read_csv(f, sep='\t', header=0) for f in glob.glob(tempdir + fnstart + '-chr*_res.txt')], sort=False, ignore_index=True)
+	resdf.to_csv(filedir + fnstart + '_results_' + starttime + '.txt', header=True, index=False, sep='\t', na_rep='N/A')
 
 	# output2 file could be an empty file, which pd.read_csv cannot read
-	op2df = pd.concat([pd.read_csv(f, sep='\t', header=None) for f in glob.glob(filedir + fnstart + '-chr*_op2.txt') if os.path.getsize(f) > 0], sort=False, ignore_index=True)
-	op2df.to_csv(filedir + fnstart + '_output2.txt', header=['Gene_ID', 'Cluster_description', 'SNP_ID', 'CLPP_at_that_SNP', 'Tissue', 'CLPP_over_the_whole_cluster'], index=False, sep='\t', na_rep='N/A')
+	op2df = pd.concat([pd.read_csv(f, sep='\t', header=None) for f in glob.glob(tempdir + fnstart + '-chr*_op2.txt') if os.path.getsize(f) > 0], sort=False, ignore_index=True)
+	op2df.to_csv(filedir + fnstart + '_output2_' + starttime + '.txt', header=['Gene_ID', 'Cluster_description', 'SNP_ID', 'CLPP_at_that_SNP', 'Tissue', 'CLPP_over_the_whole_cluster'], index=False, sep='\t', na_rep='N/A')
 
 	# remove all the intermediate files
-	for f in glob.glob(filedir + fnstart + '-chr*'): os.remove(f)
+	shutil.rmtree(tempdir)
 else:
-	print('some gave error warnings!!!')
+	print('some gave error warnings, please check!!!')
