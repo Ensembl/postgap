70139bbd05e0b038aaa728b5d4295d62a082a034
YalanBi <biyalan@yeah.net>
2020-06-04 12:22:47 +0100

    Check whether to rerun an analysis once it stops
    Change the code to check if an analysis completed successfully once it stopped running  to save time, instead of to check if all the analyses succeeded until all stopped running.

--- scripts/precheck/parallelpostgap.py
+++ scripts/precheck/parallelpostgap.py
@@ -2,7 +2,7 @@
 """
 Spyder Editor
 
-This is a script file for preworks before running postgap.py in parallel
+This is a script file to run postgap.py in parallel
 """
 
 import argparse
@@ -22,12 +22,16 @@ parser.add_argument('--database_dir', type=str, default='databases/', help='dire
 parser.add_argument('--summary_stats', required=True, type=str, help='location of input summary statistics file')
 parser.add_argument('--submit_interval', type=int, default=90, help='sleeping time (s) added to start the analysis of the next chromosome')
 parser.add_argument('--check_interval', type=int, default=300, help='sleeping time (s) added to check again if all the analyses of chromosomes have been done')
+parser.add_argument('--memory', type=int, default=2, help='request a node with how large memory (GB) to run a job')
 
 args = parser.parse_args()
 database_dir = args.database_dir
 sumstats = args.summary_stats
 submitgap = args.submit_interval
 checkgap = args.check_interval
+memory = args.memory
+
+rerunlimts = 3
 
 # load functions
 def get_checkline(filepath='dir/to/file.tsv'):
@@ -37,64 +41,50 @@ def get_checkline(filepath='dir/to/file.tsv'):
 
 	return checkline
 
-def submit_jobs(submitlist, submitgap=90, memory=str(2)):
+def submit_a_chromosome(chr, memory=str(2)):
 	"""
-	database_dir, tempdir, fnstart, filetype, starttime, checkline take global varables
+	Note: type(memory) is 'str'
+	database_dir, tempdir, fnstart, filetype, starttime take global varables
 	"""
-	if not all([os.path.exists(tempdir + fnstart + '-chr' + n + '.' + filetype) for n in submitlist]):
-		# get which colnumn is variant_id
-		ncol_rsid = str(checkline.split().index('variant_id') + 1)
-		print('column ' + ncol_rsid + ' is SNP rsid, start splitting by chromosomes and running POSTGAP:')
+	chrname = fnstart + '-chr' + chr
+	chrfile = fnstart + '-chr' + chr + '.' + filetype
+	jobname = fnstart + '-chr' + chr + '_' + starttime
 
-	for n in submitlist:
+	# res & op2 may exist when re-run jobs
+	if os.path.exists(tempdir + chrname + '_res.txt'): os.remove(tempdir + chrname + '_res.txt')
+	if os.path.exists(tempdir + chrname + '_op2.txt'): os.remove(tempdir + chrname + '_op2.txt')
+	
+	memory = str(memory)
+	os.system("gsub -m " + memory + " -n " + jobname + " -d " + tempdir + " -q production-rh74 'python postgap/POSTGAP.py --database_dir " + database_dir + " --summary_stats " + tempdir + chrfile + " --hdf5 /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5 --sqlite /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5.sqlite3 --bayesian --output " + tempdir + chrname + "_res.txt --output2 " + tempdir + chrname + "_op2.txt -g' -r")
+	
+	print(time.strftime("%H:%M:%S", time.localtime()) + ', chr' + chr + ' is submitted with ' + memory + 'G memory assigned!')
+
+def start_a_GWAS_sum_stats(chrlist, submitgap=90, memory=str(2)):
+	"""
+	Note: type(memory) is 'str'
+	tempdir, filename, fnstart, filetype, starttime, checkline take global varables
+	"""
+	# get which colnumn is variant_id
+	ncol_rsid = str(checkline.split().index('variant_id') + 1)
+	print('column ' + ncol_rsid + ' is SNP rsid, start splitting by chromosomes and running POSTGAP:')
+
+	for n in chrlist:
 		# add a sleeping time between jobs
-		if n is not submitlist[0]: 
+		if n is not chrlist[0]: 
 			print('... wait for ' + str(submitgap) + 's, then submit the next job ...')
 			time.sleep(submitgap)
 
 		chrname = fnstart + '-chr' + n
 		chrfile = fnstart + '-chr' + n + '.' + filetype
-		
 		jobname = fnstart + '-chr' + n + '_' + starttime
 
-		# create a new file for each chromosome, if it hasn't been done
-		if not os.path.exists(tempdir + chrfile):
-			os.system("(head -n1 " + tempdir + filename + " && awk 'NR==FNR {a[$3]; next} $" + ncol_rsid + " in a {print $0}' yalan/variants/variants_rsID_coords-chr" + n + ".tsv " + tempdir + filename + ") > " + tempdir + chrfile)
-		else:
-			print(chrfile + ' exists in ' + tempdir)
-
-		# res & op2 may exist when re-run jobs
-		if os.path.exists(tempdir + chrname + '_res.txt'): os.remove(tempdir + chrname + '_res.txt')
-		if os.path.exists(tempdir + chrname + '_op2.txt'): os.remove(tempdir + chrname + '_op2.txt')
-		
-		# run postgap.py ||| note: -m, -d, results & output2!!!!!!!!!!!
-		os.system("gsub -m " + memory + " -n " + jobname + " -d " + tempdir + " -q production-rh74 'python postgap/POSTGAP.py --database_dir " + database_dir + " --summary_stats " + tempdir + chrfile + " --hdf5 /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5 --sqlite /nfs/production/panda/ensembl/funcgen/eqtl/GTEx.V6.88_38.cis.eqtls.h5.sqlite3 --bayesian --output " + tempdir + chrname + "_res.txt --output2 " + tempdir + chrname + "_op2.txt -g' -r")
-		
-		print(time.strftime("%H:%M:%S", time.localtime()) + ', chr' + n + ' is submitted!')
-
-def check_jobs_status(checklist, checkgap=300):
-	"""
-	tempdir, fnstart, starttime take global varables
-	"""
-	jobstalist = [False] * len(checklist)
-
-	while not all(jobstalist):
-		# check all unfinished jobs
-		for n in [n for n,s in zip(checklist, jobstalist) if not s]:
-			jobname = fnstart + '-chr' + n + '_' + starttime
-			os.system('bjobs -J ' + jobname + ' | cat > ' + tempdir + 'jobstatus.txt')
-			#jobstatus.txt is an empty file, which means job isn't running
-			jobstalist[checklist.index(n)] = True if os.path.getsize(tempdir + 'jobstatus.txt') == 0 else False
-
-		if all(jobstalist):
-			#a job isn't running could be either finished or killed
-			print('no job is running now, check if they all quitted normally')
-			os.remove(tempdir + 'jobstatus.txt')
-			break
-		else:
-			if int(starttime[-4:]) - checkgap / 3 * 5 < int(time.strftime("%M%S", time.localtime())) < int(starttime[-4:]) + checkgap / 3 * 5:
-				print('/'.join([('chr' + n) for n,s in zip(checklist, jobstalist) if not s]) + ' unfinished, check every ' + str(checkgap) + 's')
-			time.sleep(checkgap)
+		# create a new file for each chromosome, and run postgap.py
+		os.system("(head -n1 " + tempdir + filename + " && awk 'NR==FNR {a[$3]; next} $" + ncol_rsid + " in a {print $0}' yalan/variants/variants_rsID_coords-chr" + n + ".tsv " + tempdir + filename + ") > " + tempdir + chrfile)
+		submit_a_chromosome(chr=n, memory=str(memory))
+	
+	# remove the original GWAS summary statistics file to save space
+	os.remove(tempdir + filename)
+	print(filename + ' saved in ' + tempdir + ' has been removed')
 
 def read_job_sum_file(path='dir/', whichjob='jobname', sumtype='out'):
 	f = open(path + whichjob + '.' + sumtype)
@@ -103,19 +93,118 @@ def read_job_sum_file(path='dir/', whichjob='jobname', sumtype='out'):
 
 	return text
 
-def check_a_chromosome_success(n=str(1)):
+def check_a_chromosome_success(chr):
 	"""
 	tempdir, fnstart, starttime take global varables
 	"""
-	jobname = fnstart + '-chr' + n + '_' + starttime
+	jobname = fnstart + '-chr' + chr + '_' + starttime
 
-	outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
+	success = False
+	repeat = False
 
-	if 'Successfully completed' in outtext[-23]: complete = True
-	elif 'Exited with exit code' in outtext[-23]: complete = False
-	else: complete = float('NaN') # a NaN value
+	# if jobstatus.txt is an empty file, this job isn't running; else, running
+	os.system('bjobs -J ' + jobname + ' | cat > ' + tempdir + 'jobstatus.txt')
 
-	return complete
+	# if this job is not running, check whether it finished successfully
+	if os.path.getsize(tempdir + 'jobstatus.txt') == 0:
+		# just wait for 30s, in case the stdout file has not been completed
+		time.sleep(30)
+
+		outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
+
+		if 'Successfully completed' in outtext[-23]: success = True
+		elif 'Exited with exit code' in outtext[-23]: repeat = True
+		else: # generate a NaN value, which should never happen
+			success = float('NaN')
+			print('something should never happen happened to chr ' + chr + '! that line was "' + outtext[-23].rstrip() + '" when checking')
+	
+	os.remove(tempdir + 'jobstatus.txt')
+
+	return success, repeat
+
+def increse_memory(chr):
+	"""
+	tempdir, fnstart, starttime take global varables
+	"""
+	jobname = fnstart + '-chr' + chr + '_' + starttime
+	errtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='err')
+
+	increase_mem = False
+
+	if 'HTTPError' in errtext[-1]:
+		print('a http error happened, no need to increase memory when re-run chr' + chr)
+	elif 'ConnectionError' in errtext[-1]:
+		print('a connection error happened, no need to increase memory when re-run chr' + chr)
+	elif 'IOError' in errtext[-1]:
+		print('chr' + chr + ' gave ' + errtext[-1].rstrip() + '. performance issue with the filesystem, just try again')
+	elif 'KeyboardInterrupt\n' in errtext:
+		# check stdout file to figure why it was killed
+		outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
+
+		# get Delta Memory
+		if float(outtext[-15].split()[-2]) < 0:
+			print('memory limit exceeded, will increase memory and re-run chr' + chr)
+			increase_mem = True
+		else:
+			print('warnings, warnings, warnings! chr' + chr + ' has an unknown error situation!')
+			sys.exit('unknown error situation, please debug')
+	else:
+		print('warnings, warnings, warnings! chr' + chr + ' has an unknown error situation!')
+		sys.exit('unknown error situation, please debug')
+
+	return increase_mem
+
+def trace_chromosomes_status(chrlist, init_mem=2, checkgap=checkgap, rerunlimts=3):
+	"""
+	Note: type(init_mem) is 'int'
+	tempdir, fnstart, filetype, starttime take global varables
+	"""
+	sumdf = pd.DataFrame(columns=['chr', 'success', 'repeats', 'run_time', 'cpu_time', 'max_mem', 'ave_mem'], index=range(len(chrlist)))
+	sumdf['chr'] = chrlist
+	sumdf['repeats'] = [0] * len(chrlist)
+	
+	while not all(sumdf['success'] == True):
+		# check all unfinished jobs
+		for i,row in sumdf.loc[sumdf['success'] != True].iterrows():
+			chr = row['chr']
+
+			sumdf.at[i, 'success'], repeat = check_a_chromosome_success(chr=chr)
+
+			# when one finished successfully, record relevant parameters
+			if sumdf.loc[i, 'success'] is True:
+				jobname = fnstart + '-chr' + chr + '_' + starttime
+				outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
+				
+				sumdf.ix[i, 'run_time':'ave_mem'] = [outtext[-11].split()[-2], outtext[-19].split()[-2], outtext[-18].split()[-2] if 'MB' in outtext[-18] else 'N/A', outtext[-17].split()[-2] if 'MB' in outtext[-17] else 'N/A']
+
+				# once succeeds, remove the chromosomes GWAS summary statistics file to save space
+				chrfile = fnstart + '-chr' + chr + '.' + filetype
+				os.remove(tempdir + chrfile)
+
+			if repeat:
+				sumdf.at[i, 'repeats'] += 1
+				reruntimes = sumdf.loc[i, 'repeats']
+				
+				if reruntimes <= rerunlimts:
+					print('chr' + chr + '\'s re-run round ' + str(reruntimes) + ' starts')
+					increase_mem = increse_memory(chr=chr)
+					submit_a_chromosome(chr=chr, memory=str(round(int(init_mem) * (1 + (reruntimes if increase_mem else 0) / 2))))
+
+		if all(sumdf['success'] == True):
+			print('congratulations!!! all the jobs finished successfully!')
+			break
+		elif any(sumdf['success'].isnull()):
+			print('/'.join(('chr' + n) for n in sumdf.loc[sumdf['success'].isnull()]['chr']) + ' quitted with an unknown situation!')
+			sys.exit('unknown situation occurs, please debug')
+		elif any((sumdf['success'] == False) & (sumdf['repeats'] > rerunlimts)):
+			print('/'.join(('chr' + n) for n in sumdf.loc[(sumdf['success'] == False) & (sumdf['repeats'] > rerunlimts), 'chr']) + ' has already re-ran for ' + str(rerunlimts) + ' times, some consistent error occurs!')
+			sys.exit('consistent error occurs, please debug')
+
+		if int(starttime[-4:]) - checkgap / 3 * 5 < int(time.strftime("%M%S", time.localtime())) < int(starttime[-4:]) + checkgap / 3 * 5:
+			print('/'.join(('chr' + n) for n in sumdf.loc[sumdf['success'] != True, 'chr']) + ' unfinished, check every ' + str(checkgap) + 's')
+		time.sleep(checkgap)
+	
+	return sumdf
 
 #0. preparations
 # get basic info about the GWAS summary statistics
@@ -185,87 +274,22 @@ else:
 	print('please make sure all the following information is included in your data, and please specify them with assigned column names:\nrsid -> "variant_id"\nbeta -> "beta"\np -> "p-value"')
 	sys.exit('error with column names')
 
-#4. split GWAS summary file by chromosomes and run postgap.py
+#4. split GWAS summary file by chromosomes and run postgap.py on each
 chrlist = [str(n) for n in range(1, 23)]
 chrlist.append('O')
 
-submit_jobs(submitlist=chrlist, submitgap=submitgap)
+start_a_GWAS_sum_stats(chrlist=chrlist, submitgap=submitgap, memory=str(memory))
 
-#5. wait and check if all the jobs are done
+#5. keep tracing until all the jobs completed successfully, and make a record of running/CPU time and max/average memory
 time.sleep(300)
-print('check whether all the jobs have been done ...')
-check_jobs_status(checklist=chrlist, checkgap=checkgap)
+print('start checking whether all the jobs completed successfully ...')
 
-#6. check if any has errors, and track running/CPU time
-sumdf = pd.DataFrame(columns=['chr', 'success', 'run_time', 'cpu_time', 'max_mem', 'ave_mem'], index=range(len(chrlist)))
-sumdf['chr'] = chrlist
-for i,n in enumerate(chrlist): sumdf['success'][i] = check_a_chromosome_success(n)
-
-reruntimes = 0
-increase_mem = False
-while not all(sumdf['success']):
-	if reruntimes == 3:
-		print('already re-run for 3 times, some consistent error occurs!')
-		sys.exit('consistent error occurs, please debug')
-	
-	if sumdf['success'].isnull().values.any():
-		print('/'.join(('chr' + n) for n in sumdf.loc[sumdf['success'].isnull()]['chr']) + ' has an unknown situation!')
-		sys.exit('unknown situation occurs, please debug')
-	
-	rerunlist = []
-	for n in sumdf.loc[sumdf['success'] == False]['chr']:
-		jobname = fnstart + '-chr' + n + '_' + starttime
-
-		errtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='err')
-
-		if 'HTTPError' in errtext[-1]:
-			print('a connection error happened, will re-run chr' + n)
-			rerunlist.append(n)
-		elif 'KeyboardInterrupt\n' in errtext:
-			outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
-
-			# get Delta Memory
-			if float(outtext[-15].split()[-2]) < 0:
-				print('memory limit exceeded, will increase memory and re-run chr' + n)
-				rerunlist.append(n)
-				increase_mem = True
-			else:
-				print('warnings, warnings, warnings! chr' + n + ' has an unknown error situation!')
-		else:
-			print('warnings, warnings, warnings! chr' + n + ' has an unknown error situation!')
-	
-	# if all failed jobs have unexpected errors, stop and debug
-	if len(rerunlist) == 0:
-		print('/'.join(('chr' + n) for n in sumdf.loc[sumdf['success'] == False]['chr']) + ' has an unknown error situation! warnings, warnings, warnings!')
-		sys.exit('unknown error to debug')
-	
-	reruntimes += 1
-	submit_jobs(submitlist=rerunlist, submitgap=submitgap, memory=str(2 + (reruntimes if increase_mem else 0)))
-	print('re-run round ' + str(reruntimes) + ': ' + '/'.join(('chr' + n) for n in rerunlist) + ' starts! allocate ' + str(2 + (reruntimes if increase_mem else 0)) + 'G memory')
-	
-	time.sleep(300)
-	print('check whether all the re-run jobs have been done ...')
-	check_jobs_status(checklist=rerunlist, checkgap=checkgap)
-	
-	# update sumdf['success']
-	for i,row in sumdf.loc[sumdf['chr'].isin(rerunlist)].iterrows(): sumdf.at[i, 'success'] = check_a_chromosome_success(row['chr'])
-
-print('congratulations!!! all the jobs are finished successfully! let\'s track relevant parameters and wrap all the results together')
-
-for i,n in enumerate(chrlist):
-	jobname = fnstart + '-chr' + n + '_' + starttime
-	
-	outtext = read_job_sum_file(path=tempdir, whichjob=jobname, sumtype='out')
-
-	sumdf.at[i, 'run_time'] = outtext[-11].split()[-2]
-	sumdf.at[i, 'cpu_time'] = outtext[-19].split()[-2]
-	sumdf.at[i, 'max_mem'] = outtext[-18].split()[-2] if 'MB' in outtext[-18] else 'N/A'
-	sumdf.at[i, 'ave_mem'] = outtext[-17].split()[-2] if 'MB' in outtext[-17] else 'N/A'
-
+sumdf = trace_chromosomes_status(chrlist=chrlist, init_mem=memory, checkgap=checkgap, rerunlimts=rerunlimts)
 sumdf.to_csv(filedir + fnstart + '_jobsum_' + starttime + '.txt', header=True, index=False, sep='\t', na_rep='N/A')
 
-#7. combine all the results into one file
-print('all the jobs have been completed successfully, and wrap all the results together')
+#6. combine all the results into one file
+print('wrap all the results together, and clean up')
+
 # results file could have only column names, and pd.concat can combine even empty dfs
 resdf = pd.concat([pd.read_csv(f, sep='\t', header=0, low_memory=False) for f in glob.glob(tempdir + fnstart + '-chr*_res.txt')], sort=False, ignore_index=True)
 resdf.to_csv(filedir + fnstart + '_results_' + starttime + '.txt', header=True, index=False, sep='\t', na_rep='N/A')
@@ -275,4 +299,5 @@ op2df = pd.concat([pd.read_csv(f, sep='\t', header=None, low_memory=False) for f
 op2df.to_csv(filedir + fnstart + '_output2_' + starttime + '.txt', header=['Gene_ID', 'Cluster_description', 'SNP_ID', 'CLPP_at_that_SNP', 'Tissue', 'CLPP_over_the_whole_cluster'], index=False, sep='\t', na_rep='N/A')
 
 # remove all the intermediate files
-shutil.rmtree(tempdir)
+for f in glob.glob(tempdir + fnstart + '-chr*_*.txt'): os.remove(f)
+#shutil.rmtree(tempdir)
