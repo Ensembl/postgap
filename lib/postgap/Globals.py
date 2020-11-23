28ccbce7fa67048e7d13f80116876c6a3cc2deb1
Yalan Bi <yalan@noah-login-01.ebi.ac.uk>
2020-11-23 16:43:33 +0000

    update p-value significance threshold to be the commonly used standard one

--- lib/postgap/Globals.py
+++ lib/postgap/Globals.py
@@ -61,7 +61,7 @@ work_directory = "notset"
 finemap_gwas_clusters_directory = None
 finemap_eqtl_clusters_directory = None
 
-GWAS_PVALUE_CUTOFF = 1e-4
+GWAS_PVALUE_CUTOFF = 5*10**(-8) #1e-4
 
 GWAS_SUMMARY_STATS_FILE = None
 
