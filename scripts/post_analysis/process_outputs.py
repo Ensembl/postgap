2d962c0b6c515f96617c3417c3500182e15eff2f
YalanBi <biyalan@yeah.net>
2021-02-09 12:41:06 +0000

    Update process_outputs.py

--- scripts/post_analysis/process_outputs.py
+++ scripts/post_analysis/process_outputs.py
@@ -7,7 +7,6 @@ This is a script file to process the outputs of an analysis to create the datafr
 
 
 
-import os
 import pandas as pd
 
 # get command line arguments
@@ -17,12 +16,11 @@ parser.add_argument('--starttime', type=str, default='210130231014', help='the t
 args = parser.parse_args()
 starttime = args.starttime
 
-os.chdir('test_' + starttime + '/')
-#os.chdir('C:/Users/yalan/Documents/POSTGAP/forNovonordisk/test_' + starttime + '/')
+tempdir = 'yalan/NNRtest/'
 
 # load in results & output2 files
-df_res = pd.read_csv('CAD_UKBIOBANK_results_ks1_km2_spcl_' + starttime + '.txt', delimiter='\t', header=0, low_memory=False)
-df_op2 = pd.read_csv('CAD_UKBIOBANK_output2_ks1_km2_spcl_' + starttime + '.txt', delimiter='\t', header=0, low_memory=False)
+df_res = pd.read_csv(tempdir + 'CAD_UKBIOBANK_results_ks1_km2_spcl_' + starttime + '.txt', delimiter='\t', header=0, low_memory=False)
+df_op2 = pd.read_csv(tempdir + 'CAD_UKBIOBANK_output2_ks1_km2_spcl_' + starttime + '.txt', delimiter='\t', header=0, low_memory=False)
 df_op2.columns = ['gene_id', 'cluster', 'rsID', 'CLPP_SNP', 'tissue', 'CLPP_cluster']
 
 # list SNP - associated gene pairs in results
@@ -30,7 +28,7 @@ snp_gene_pairs = df_res[['ld_snp_rsID', 'gene_id']].drop_duplicates()
 
 # refine output2: remove SNP - gene pairs that in output2 but not in results
 df_plot = df_op2.loc[[any((snp_gene_pairs['ld_snp_rsID'] == row['rsID']) & (snp_gene_pairs['gene_id'] == row['gene_id'])) for index, row in df_op2.iterrows()]]
-df_plot.to_csv('CAD_UKBIOBANK_df_plot_' + starttime + '.tsv', sep='\t', header=True, index=False)
+df_plot.to_csv(tempdir + 'CAD_UKBIOBANK_df_plot_' + starttime + '.tsv', sep='\t', header=True, index=False)
 
-# run under the directory yalan/NNRtest/
-#gsub -m 50 -n df_plot -d . -q production-rh74 'python process_outputs.py --starttime 210130231014' -r
+# run under the home directory
+#gsub -m 50 -n df_plot -d yalan/NNRtest/ -q production-rh74 'python postgap/scripts/post_analysis/process_outputs.py --starttime 210208123530' -r
