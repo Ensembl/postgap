f3c01afd527315000baee84034b576013eb93e28
jcmarca <jcmarca@ebi.ac.uk>
2022-01-18 12:44:38 +0000

    Add Hardcoded list of tissues and update p-values in the example file

--- lib/postgap/Integration.py
+++ lib/postgap/Integration.py
@@ -824,10 +824,6 @@ def regulatory_evidence(snps):
 	return hash
 
 def get_all_tissues():
-	server = 'http://rest.ensembl.org'
-	ext = '/eqtl/tissue/homo_sapiens?content-type=application/json'
-	try:
-		return postgap.REST.get(server, ext)
+        tissues = ["Adipose_Subcutaneous", "Adipose_Visceral_Omentum", "Adrenal_Gland", "Artery_Aorta", "Artery_Coronary", "Artery_Tibial", "Brain_Anterior_cingulate_cortex_BA24", "Brain_Caudate_basal_ganglia", "Brain_Cerebellar_Hemisphere", "Brain_Cerebellum", "Brain_Cortex", "Brain_Frontal_Cortex_BA9", "Brain_Hippocampus", "Brain_Hypothalamus", "Brain_Nucleus_accumbens_basal_ganglia", "Brain_Putamen_basal_ganglia", "Breast_Mammary_Tissue", "Cells_EBV-transformed_lymphocytes", "Cells_Transformed_fibroblasts", "Colon_Sigmoid", "Colon_Transverse", "Esophagus_Gastroesophageal_Junction", "Esophagus_Mucosa", "Esophagus_Muscularis", "Heart_Atrial_Appendage", "Heart_Left_Ventricle", "Liver", "Lung", "Muscle_Skeletal", "Nerve_Tibial", "Ovary", "Pancreas", "Pituitary", "Prostate", "Skin_Not_Sun_Exposed_Suprapubic", "Skin_Sun_Exposed_Lower_leg", "Small_Intestine_Terminal_Ileum", "Spleen", "Stomach", "Testis", "Thyroid", "Uterus", "Vagina", "Whole_Blood"]
 
-	except:
-		return None
+        return tissues
